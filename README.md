139f5695b8bae3b5a817867c8276279b40a2065d
yongun2 <djkye0001@naver.com>
2023-06-27 18:35:45 +0900

    Project Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Todo List
+
+## 목표
+
+- DRF Todo List 서버와 연동하기
+- 3가지의 상태관리 모두 사용해보기
+  - only useState
+  - redux
+  - recoil
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function App() {
+  return <></>;
+}
+
+export default App;
